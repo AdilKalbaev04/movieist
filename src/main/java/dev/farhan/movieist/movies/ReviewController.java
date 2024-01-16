598324297b154212571a11a3a4bfb0e0c6e78f7f
AdilKalbaev04 <adilkalbaev2004@gmail.com>
2024-01-16 21:05:35 +0600

    add get mapping for reviews

--- src/main/java/dev/farhan/movieist/movies/ReviewController.java
+++ src/main/java/dev/farhan/movieist/movies/ReviewController.java
@@ -6,17 +6,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
+
     @Autowired
     private ReviewService service;
 
+    @Autowired
+    private ReviewRepository repository;
+
     @PostMapping()
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<>(service.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.OK);
+    }
 
-        return new ResponseEntity<Review>(service.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.OK);
+    @GetMapping()
+    public ResponseEntity<List<Review>> getAllReviews() {
+        List<Review> reviews = repository.findAll();
+        return new ResponseEntity<>(reviews, HttpStatus.OK);
     }
 }
+
