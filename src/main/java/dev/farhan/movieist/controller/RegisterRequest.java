ba7911e2d2fae3f8a2d287fe887df1e6d72086ab
AdilKalbaev04 <adilkalbaev2004@gmail.com>
2024-02-04 21:28:46 +0600

    use lombok

--- src/main/java/dev/farhan/movieist/controller/RegisterRequest.java
+++ src/main/java/dev/farhan/movieist/controller/RegisterRequest.java
@@ -1,22 +1,12 @@
 package dev.farhan.movieist.controller;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class RegisterRequest {
     private String username;
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
